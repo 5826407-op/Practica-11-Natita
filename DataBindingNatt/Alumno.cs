f9704ba02427025fb7e4c5182f5aaf5df8feeaa5
Nathalia <5826407@clases.edu.sv>
2026-09-17 09:46:41 -0600

    Subiendo DataBindingNatt

---
+++ DataBindingNatt/Alumno.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace DataBindingNatt
+{
+    public class Alumno : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string nombre = string.Empty;
+
+        public string Nombre
+        {
+            get => nombre;
+            set
+            {
+                if (nombre == value)
+                    return;
+                
+                nombre = value;
+                onPropertyChanged(nameof(Nombre));
+                onPropertyChanged(nameof(MostrarNombre));
+            }
+        }
+
+        public string MostrarNombre => $"Nombre ingresado: {Nombre}";
+
+        void onPropertyChanged(string nombrePropiedad)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+        }
+    }
+}
---
+++ DataBindingNatt/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBindingNatt
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
